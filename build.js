3c51fe5352920184103ccdf1d845f4604256b54f
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-08-26 20:17:17 -0400

    Update banner

--- build.js
+++ build.js
@@ -5,6 +5,7 @@ const sass = require('sass');
 const esbuild = require('esbuild');
 const hljs = require('highlight.js/package.json');
 
+
 // --> Set path constants
 const worktDir = __dirname;
 const srcDir = path.join(worktDir, 'src/');
@@ -12,17 +13,30 @@ const distDir = path.join(worktDir, 'dist/');
 const customFile = path.join(srcDir, 'jscripts/highlight-custom.js');
 const outputFile = path.join(distDir, 'highlight.plugin.min.js');
 
+
+// --> Get Now date
+const d = new Date();
+const pad = n => String(n).padStart(2, '0');
+const now = `${d.getFullYear()}-${pad(d.getMonth()+1)}-${pad(d.getDate())} ${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;
+
+
 // --> Load template
 const templateContent = fs.readFileSync(path.join(srcDir, 'jscripts/highlight-template.js'), 'utf8');
+
 
 // --> Compile CSS
 const cssContent = sass.compile(path.join(srcDir, 'styles/styles.scss'), { style: 'compressed', sourceMap: false}).css;
 
+
 // --> Load Plugin JS
 const pluginContent = fs.readFileSync(path.join(srcDir, 'jscripts/highlight-pxdoc.js'), 'utf8');
 
+
 // --> Load Banner Content
-const bannerContent = fs.readFileSync(path.join(srcDir, 'banner.txt'), 'utf8').replace(/###VERSION###/i, hljs.version);
+const bannerContent = fs.readFileSync(path.join(srcDir, 'banner.txt'), 'utf8')
+.replace(/###VERSION###/i, hljs.version)
+.replace(/###DATETIME###/i, now);
+
 
 // --> Compile Costum Highlight.js
 const highlightContent = esbuild.buildSync({
@@ -38,13 +52,15 @@ const highlightContent = esbuild.buildSync({
     write: false
 }).outputFiles[0].text;
 
+
 // --> Bundle all files
 const bundleContent = templateContent
     .replace(/###CSSCONTENT###/i, cssContent)
     .replace(/###HIGHLIGHTCONTENT###/i, highlightContent)
     .replace(/###PLUGINCONTENT###/i, pluginContent);
 
-// --> Build final plugin
+
+    // --> Build final plugin
 esbuild.build({
     stdin: { contents: bundleContent },
     banner: { js: bannerContent },
@@ -61,8 +77,3 @@ esbuild.build({
     console.error('❌ Erreur ESBuild :', err.message);
     process.exit(1);
 });
-
-
-
-
-
